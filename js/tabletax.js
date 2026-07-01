f8e9d10d5e986f9bb983ae88e0128ed7673a3450
djmp110 <mutuyipaci@gmail.com>
2026-07-01 22:17:49 +0200

    js separation

---
+++ js/tabletax.js
@@ -0,0 +1,32 @@
+ var tax = 0.23;
+    var tbody = document.getElementById("tax");
+    var rows = "";
+
+    for (var i = 0; i < 10; i++) {
+
+        var workername = prompt("Enter employee name:");
+        var job = prompt("Enter employee job:");
+        var grossSalary = parseFloat(prompt("Enter employee salary:"));
+
+        var taxallow;
+        var netsalary;
+
+        if (grossSalary < 70000) {
+            taxallow = "No";
+            netsalary = grossSalary;
+        } else {
+            taxallow = "Yes";
+            netsalary = grossSalary - (grossSalary * tax);
+        }
+
+        rows += `
+            <tr>
+                <td>${i + 1}</td>
+                <td>${workername}</td>
+                <td>${job}</td>
+                <td>${grossSalary.toFixed(2)} Rwf</td>
+                <td>${taxallow}</td>
+                <td>${netsalary.toFixed(2)} Rwf</td>
+            </tr>
+        `;
+    }
